49e28eda831e3d0eb571bee8cc8dfc72fc65bda1
Dan Koller <57103678+dan-koller@users.noreply.github.com>
2024-03-04 15:33:55 +0100

    Fixed Progressbar rendering issues on mac

--- NScan.Cli/CliScanner.cs
+++ NScan.Cli/CliScanner.cs
@@ -1,5 +1,6 @@
 ﻿using NScan.Core;
 using System.Net;
+using NScan.Cli.Rendering;
 
 namespace NScan.Cli;
 
@@ -9,7 +10,6 @@ public class CliScanner
     private int _startPort;
     private int _endPort;
     private int _timeoutMilliseconds;
-    private float _progress;
 
     public CliScanner(string target, int startPort, int endPort, int timeoutMilliseconds)
     {
@@ -26,13 +26,10 @@ public class CliScanner
 
         ScanService scanService = new ScanService(ipAddress, _startPort, _endPort, _timeoutMilliseconds);
         ProgressService progressService = new ProgressService(scanService, _startPort, _endPort);
-        // Start the rendering of progress bars asynchronously
+
+        // Scanning and progress rendering are independent tasks
         Task progressTask = RenderProgress(progressService);
-
-        // Start scanning ports asynchronously
         Task<List<int>> scanTask = scanService.ScanPorts(scanMethod);
-
-        // Wait for both tasks to complete
         await Task.WhenAll(progressTask, scanTask);
 
         return await scanTask;
@@ -40,34 +37,18 @@ public class CliScanner
 
     public async Task RenderProgress(ProgressService progressService)
     {
-        // Update the progress continuously until scanning is complete
+        using var progressBar = new ProgressBar();
         while (true)
         {
-            // Get the current progress
             float progress = progressService.GetProgress();
 
             // Render the progress bar
-            RenderProgressBar(progress);
+            progressBar.Report(progress);
 
             // Check if scanning is complete
-            if (progress >= 1.0f)
-                break;
+            if (progress >= 1.0f) break;
 
-            // Wait for a short interval before updating again
             await Task.Delay(100);
         }
-    }
-    public void RenderProgressBar(float progress)
-    {
-        // Calculate the width of the progress bar
-        int barWidth = Console.WindowWidth - 10;
-        int progressWidth = (int)(barWidth * progress);
-
-        // Render the progress bar
-        Console.Write("[");
-        Console.Write(new string('=', progressWidth));
-        Console.Write(new string(' ', barWidth - progressWidth));
-        Console.Write($"] {progress:P}");
-        Console.CursorLeft = 0; // Move the cursor to the beginning of the line
     }
 }
