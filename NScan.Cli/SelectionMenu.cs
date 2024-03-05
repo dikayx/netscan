7affdb004bf7138e74241b3ecf168376307fe1e4
Dan Koller <57103678+dan-koller@users.noreply.github.com>
2024-03-05 18:38:08 +0100

    back to simple menu

--- NScan.Cli/SelectionMenu.cs
+++ NScan.Cli/SelectionMenu.cs
@@ -12,15 +12,13 @@ public class SelectionMenu(string banner, string[] options)
         Clear();
         OutputEncoding = Encoding.UTF8;
         CursorVisible = false;
-        var consoleColor = GetRandomConsoleColor;
-        ForegroundColor = consoleColor;
+        ForegroundColor = ConsoleColor.White;
         WriteLine(_banner);
         ResetColor();
         WriteLine("\nUse ↑ and ↓ to navigate and press Enter/Return to select a scan method\n");
         (int left, int top) = GetCursorPosition();
         var option = 0;
-        //var decorator = "\u001b[32m\u001b[1m>> "; // green
-        var decorator = ConsoleColorToAnsi(consoleColor) + "\u001b[1m>> ";
+        var decorator = "\u001b[32m\u001b[1m>> "; // green
         ConsoleKeyInfo key;
         bool isSelected = false;
 
@@ -54,22 +52,5 @@ public class SelectionMenu(string banner, string[] options)
 
         CursorVisible = true;
         return option;
-    }
-
-    // Little gimmick: Get a random color for the banner
-    private static ConsoleColor GetRandomConsoleColor => (ConsoleColor)new Random().Next(1, 8);
-
-    static string ConsoleColorToAnsi(ConsoleColor color)
-    {
-        return color switch
-        {
-            ConsoleColor.DarkBlue => "\u001b[34m",
-            ConsoleColor.DarkGreen => "\u001b[32m",
-            ConsoleColor.DarkCyan => "\u001b[36m",
-            ConsoleColor.DarkRed => "\u001b[31m",
-            ConsoleColor.DarkMagenta => "\u001b[35m",
-            ConsoleColor.DarkYellow => "\u001b[33m",
-            _ => "\u001b[32m",// default to green
-        };
     }
 }
