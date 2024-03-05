fc5d7eba720d698d866a899bf51ca82742a3feca
Dan Koller <57103678+dan-koller@users.noreply.github.com>
2024-03-05 16:51:51 +0100

    Added a neat little random color theme to the menu

--- NScan.Cli/SelectionMenu.cs
+++ NScan.Cli/SelectionMenu.cs
@@ -1,49 +1,75 @@
-﻿namespace NScan.Cli;
+using System.Text;
 
-public class SelectionMenu(string[] options)
+namespace NScan.Cli;
+
+public class SelectionMenu(string banner, string[] options)
 {
+    private readonly string _banner = banner;
     private readonly string[] _options = options;
 
     public int ShowMenu()
     {
-        int selectedIndex = 0;
-        int topPosition = CursorTop;
+        Clear();
+        OutputEncoding = Encoding.UTF8;
+        CursorVisible = false;
+        var consoleColor = GetRandomConsoleColor;
+        ForegroundColor = consoleColor;
+        WriteLine(_banner);
+        ResetColor();
+        WriteLine("\nUse ↑ and ↓ to navigate and press Enter/Return to select a scan method\n");
+        (int left, int top) = GetCursorPosition();
+        var option = 0;
+        //var decorator = "\u001b[32m\u001b[1m>> "; // green
+        var decorator = ConsoleColorToAnsi(consoleColor) + "\u001b[1m>> ";
+        ConsoleKeyInfo key;
+        bool isSelected = false;
 
-        ConsoleKeyInfo keyInfo;
-        do
+        while (!isSelected)
         {
-            SetCursorPosition(0, topPosition);
+            SetCursorPosition(left, top);
+
             for (int i = 0; i < _options.Length; i++)
             {
-                SetCursorPosition(0, topPosition + i);
-                if (i == selectedIndex)
-                {
-                    ForegroundColor = ConsoleColor.Green;
-                    Write(">> ");
-                }
-                else
-                {
-                    Write("   ");
-                }
-                WriteLine(_options[i]);
-                ResetColor();
+                WriteLine($"{(option == i ? decorator : "   ")}{_options[i]}\u001b[0m");
             }
 
-            keyInfo = ReadKey(true);
+            key = ReadKey(false);
 
-            if (keyInfo.Key == ConsoleKey.UpArrow)
+            int optionsCount = _options.Length - 1;
+            switch (key.Key)
             {
-                selectedIndex = (selectedIndex - 1 + _options.Length) % _options.Length;
-            }
-            else if (keyInfo.Key == ConsoleKey.DownArrow)
-            {
-                selectedIndex = (selectedIndex + 1) % _options.Length;
-            }
+                case ConsoleKey.UpArrow:
+                    option = option == 0 ? optionsCount : option - 1;
+                    break;
 
-        } while (keyInfo.Key != ConsoleKey.Enter);
+                case ConsoleKey.DownArrow:
+                    option = option == optionsCount ? 0 : option + 1;
+                    break;
 
-        WriteLine(); // Move to the next line after selecting an option
-        return selectedIndex;
+                case ConsoleKey.Enter:
+                    isSelected = true;
+                    break;
+            }
+        }
+
+        CursorVisible = true;
+        return option;
     }
 
+    // Little gimmick: Get a random color for the banner
+    private static ConsoleColor GetRandomConsoleColor => (ConsoleColor)new Random().Next(1, 7);
+
+    static string ConsoleColorToAnsi(ConsoleColor color)
+    {
+        return color switch
+        {
+            ConsoleColor.DarkBlue => "\u001b[34m",
+            ConsoleColor.DarkGreen => "\u001b[32m",
+            ConsoleColor.DarkCyan => "\u001b[36m",
+            ConsoleColor.DarkRed => "\u001b[31m",
+            ConsoleColor.DarkMagenta => "\u001b[35m",
+            ConsoleColor.DarkYellow => "\u001b[33m",
+            _ => "\u001b[32m",// default to green
+        };
+    }
 }
